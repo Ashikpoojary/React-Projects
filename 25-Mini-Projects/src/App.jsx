54d2a0c26a5d17b8bb7640486cb4370b5aeff724
Ashikpoojary <ashikpoojary06@gmail.com>
2024-06-07 13:23:03 +0530

    TreeView-QrCode-SwitchTheme

--- 25-Mini-Projects/src/App.jsx
+++ 25-Mini-Projects/src/App.jsx
@@ -7,17 +7,24 @@ import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
 import ImageSlider from './components/image-slider'
 import LoadMoreData from './components/load-more-data'
+import TreeView from './components/tree-view'
+import menus from "./components/tree-view/data";
+import QRCodeGenerator from './components/qr-code-generator'
+import LightDarkMode from './components/light-dark-mode'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    {/* <Accordian></Accordian>
-    <RandomColor></RandomColor>
-    <StarRating noOfStars={10}/>
-    <ImageSlider url={`https://picsum.photos/v2/list`} limit={'10'} page={1}></ImageSlider> 
-    <LoadMoreData></LoadMoreData> */}
+    {/* <Accordian></Accordian> */}
+    {/* <RandomColor></RandomColor> */}
+    {/* <StarRating noOfStars={10}/> */}
+    {/* <ImageSlider url={`https://picsum.photos/v2/list`} limit={'10'} page={1}></ImageSlider>  */}
+    {/* <LoadMoreData></LoadMoreData>  */}
+    {/* <TreeView menus={menus}/>  */}
+    {/* <QRCodeGenerator></QRCodeGenerator> */}
+    <LightDarkMode></LightDarkMode>
     </>
   )
 }
